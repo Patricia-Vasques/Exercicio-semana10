3f509f83f8026916fd095e2caf730fa8bc66eb71
Patrícia Vasques <patriciahhhh@gmail.com>
2023-04-08 16:50:38 -0300

    Atividade-6

--- formulario/src/App.jsx
+++ formulario/src/App.jsx
@@ -1,13 +1,25 @@
+import React from "react"
 import DadosPessoais from "./components/DadosPessoais"
+import Endereco from "./components/Endereco"
+import Agradecimento from "./components/Agradecimento"
 
 
 function App() {
- 
+
+  const componentesFormulario = [<DadosPessoais />, <Endereco />, <Agradecimento />]
+
+  const { passoAtual, componenteAtual, mudarPasso, ehUltimoPasso } = useForm(componentesFormulario)
 
   return (
-    <div className="App">
+    <div className="app">
 
-    </div>
+      <SCFormContainer>
+        <form onSubmit={(e) => mudarPasso(passoAtual + 1, e)}>
+          <Passos passoAtual={passoAtual}/>
+          <div className="inputs-container">{componenteAtual}</div>
+        </form>
+        </SCFormContainer>
+      </div>
   )
 }
 
